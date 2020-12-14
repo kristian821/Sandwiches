2f733fbb4620f4962b154e05cdb2fe15b3922ae4
kristian821 <56667566+kristian821@users.noreply.github.com>
2020-12-13 21:05:23 -0800

    Initial Commit

---
+++ Shared/SandwichesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandwichesApp.swift
+//  Shared
+//
+//  Created by Kristian Mitchell on 12/13/20.
+//
+
+import SwiftUI
+
+@main
+struct SandwichesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
